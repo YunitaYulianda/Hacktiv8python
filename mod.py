a5c150641539cb433ed30934ad7b0de58f3e2bfa
YunitaYulianda <yunitayulianda2019@gmail.com>
2023-06-16 21:35:15 +0700

    latihan

---
+++ mod.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s = "Tutorial Python Kampus Merdeka"
+mylist = [10,20,30,40]
+
+def fungsi_kostum(x,y,z):
+    return x**2 + y*z
+
+print(fungsi_kostum(2,3,4))
+print("Ini menjalankan mod.py")
+
+
+# In[ ]:
+
+
+
+
